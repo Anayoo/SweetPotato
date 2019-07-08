53bceb8280e9d37cfd7f020f53ad2dc968021ca1
Anayoo <psite1@126.com>
2019-07-08 11:20:13 +0800

    优化docker build逻辑

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /usr/local/SweetPotato
 ARG JAR_FILE
 COPY target/${JAR_FILE} app.jar
 COPY config config
+RUN unzip app.jar > /dev/null; rm BOOT-INF/classes/application.yml; rm BOOT-INF/classes/potatoes.xml
 COPY start.sh start.sh
 
 EXPOSE 8080
--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-unzip app.jar > /dev/null
-rm BOOT-INF/classes/application.yml
-rm BOOT-INF/classes/potatoes.xml
-
 args=`ls BOOT-INF/lib/ | xargs -i echo -n ":BOOT-INF/lib/{}" | cut -c 2-`
 java -classpath config:BOOT-INF/classes:$args cn.anayoo.sweetpotato.ClientApplication
